82a5006e5b7b16b962d56de4b4b602a39e4723ab
Anny Confessor <anny.confessor@gmail.com>
2021-07-14 17:55:19 -0300

    feat: add initial routes

---
+++ src/Routes.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './screens/Home';
+
+const Routes = (): JSX.Element => (
+  <Router>
+      <Switch>
+          <Route exact path="/" component={Home} />
+      </Switch>
+  </Router>
+);
+
+export default Routes;
