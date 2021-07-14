042cfc1f5e9162e904372107273a723f2d7144d5
Anny Confessor <anny.confessor@gmail.com>
2021-07-14 18:48:02 -0300

    feat: My404Page route and NotFoundPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,11 +6,13 @@ import {
   Route,
 } from 'react-router-dom';
 import Home from './screens/Home';
+import My404Component from './screens/My404Component';
 
 const Routes = (): JSX.Element => (
   <Router>
       <Switch>
           <Route exact path="/" component={Home} />
+          <Route path='*' exact={true} component={My404Component} />
       </Switch>
   </Router>
 );
